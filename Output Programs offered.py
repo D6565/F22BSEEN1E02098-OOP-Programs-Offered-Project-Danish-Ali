4237f58e4a45dd3e773de9786d0183ee27672586
Danish Ali <116807372+D6565@users.noreply.github.com>
2023-04-28 20:51:55 +0500

    Add files via upload
    Output Programs Offered Module

---
+++ Output Programs offered.py
@@ -0,0 +1,9 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: D:\Softwaresssss\Sublime Text 3\My program.py ============
+Programs_Offered:
+CS101 - Introduction to Computer Science (1 units)
+ENG101 - Composition I (2 units)
+SE202 - Software Engineering (3 units)
+Which Programs You Select: 1,2,3
